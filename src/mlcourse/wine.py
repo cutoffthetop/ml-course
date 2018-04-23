1fbc7f5463c919541ebccf59a9f1af29980c83a0
Nicolas Drebenstedt <cutoffthetop@me.com>
2018-04-23 13:11:46 +0200

    Randomly search parameter space for wine experiment

--- src/mlcourse/wine.py
+++ src/mlcourse/wine.py
@@ -1,12 +1,17 @@
 import numpy as np
 import graphviz
+from pprint import pprint
 import matplotlib.pyplot as plt
-from sklearn import datasets, metrics, tree
+from sklearn import datasets, metrics, tree, model_selection
 
-x, y = datasets.load_wine(True)
+wine = datasets.load_wine()
+
+x = wine.data
+y = wine.target
+zipped = list(zip(x, y))
+np.random.shuffle(zipped)
+x, y = zip(*zipped)
 threshold = int(np.floor(len(x) * .85))
-np.random.shuffle(x)
-np.random.shuffle(y)
 x_train = x[:threshold]
 x_test = x[threshold:]
 y_train = y[:threshold]
@@ -15,9 +20,29 @@ y_test = y[threshold:]
 dt = tree.DecisionTreeClassifier()
 dt = dt.fit(x_train, y_train)
 
-y_predict = dt.predict(x_test)
+random_grid = dict(
+    criterion=['gini', 'entropy'],  # quality of a split
+    splitter=['best', 'random'],  # split strategy
+    max_depth=[2, 5, 10, 20],  # maximum depth of the tree
+    min_samples_split=[2, 0.2, 0.6, 1.0],  # min samples to split node
+    min_samples_leaf=[1, 0.1, 0.3, 0.5],  # min leaf nodes required
+    min_weight_fraction_leaf=[0.0, 0.25, 0.5],  # minimum weighted fraction
+    max_features=['auto', 'sqrt', 'log2', None],  # features considered
+    max_leaf_nodes=[None, 2, 5, 10],  # max leaf nodes
+    presort=[True, False])  # presort data before fitting
+dt_random = model_selection.GridSearchCV(
+    estimator=dt,
+    param_grid=random_grid,
+    error_score=0,
+    verbose=1,
+    n_jobs=4)
+dt_random.fit(x_train, y_train)
+pprint(dt_random.best_params_)
 
-score = metrics.f1_score(y_test, y_predict, average='weighted')
+y_predict = dt.predict(x_test)
+accuracy = metrics.accuracy_score(y_test, y_predict)
+print('accuracy', accuracy)
+
 
 def main():
-    print('f1_score', score)
+    print(__name__)
