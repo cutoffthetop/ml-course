bd7af3d867919fc463e97e0fbefbf111bb250050
Nicolas Drebenstedt <cutoffthetop@me.com>
2018-05-21 11:05:39 +0200

    Improve parameters and add confusion matrix

--- src/mlcourse/digits.py
+++ src/mlcourse/digits.py
@@ -3,22 +3,21 @@ from keras.layers import Dense, Dropout
 from keras.models import Sequential
 from keras.utils.np_utils import to_categorical
 from sklearn import datasets
+from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
-from sklearn.preprocessing import LabelEncoder, MinMaxScaler
+from sklearn.preprocessing import MinMaxScaler
 
 # load digits dataset
 digits = datasets.load_digits()
 x = digits.data
 y = digits.target
 
-# encode to using one-hot label encoder
-encoder = LabelEncoder()
-encoder.fit(y)
-y = to_categorical(encoder.transform(y))
+# encode labels to one-hot
+y = to_categorical(y)
 
-# in-place scaling of features to range(0, 1)
-MinMaxScaler(copy=False).fit_transform(x)
+# scaling of features to fit range 0-1
+x = MinMaxScaler().fit_transform(x)
 
 # shuffle and split into training and test
 x, x_test, y, y_test = train_test_split(
@@ -29,7 +28,7 @@ x, x_test, y, y_test = train_test_split(
 
 # fit multi-layer perceptron classifier
 mlp = MLPClassifier(
-    hidden_layer_sizes=(64, 32, 32, 10),
+    hidden_layer_sizes=(64, 48, 10),
     activation='relu',
     solver='adam',
     max_iter=250,
@@ -41,19 +40,31 @@ mlp = mlp.fit(x, y)
 mlp_accuracy = mlp.score(x_test, y_test)
 print('sklearn', mlp_accuracy)
 
+# calculcate confusion matrix for predicted labels
+label_pred = np.argmax(mlp.predict(x_test), axis=1)
+label_true = np.argmax(y_test, axis=1)
+cf_matrix = confusion_matrix(label_true, label_pred)
+print(cf_matrix)
+
 # construct multi-layer keras network
 seq = Sequential()
-seq.add(Dense(32, activation='relu', input_dim=64))
-seq.add(Dense(32, activation='relu'))
-seq.add(Dense(10, activation='softplus'))
+seq.add(Dense(48, activation='relu', input_dim=64))
+seq.add(Dense(10, activation='softmax'))
 seq.compile(
-    loss='mean_squared_error',
+    loss='mean_absolute_error',
     optimizer='adam',
     metrics=['accuracy'])
 seq.fit(x, y, epochs=250)
 
+# output mean keras network accuray
 _, seq_accuracy = seq.evaluate(x_test, y_test)
 print('keras', seq_accuracy)
+
+# calculcate confusion matrix for predicted labels
+label_pred = np.argmax(seq.predict(x_test), axis=1)
+label_true = np.argmax(y_test, axis=1)
+cf_matrix = confusion_matrix(label_true, label_pred)
+print(cf_matrix)
 
 def main():
     print(__name__)
